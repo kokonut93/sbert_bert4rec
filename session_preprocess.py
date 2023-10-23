65f3a6322122e936f0bf3be7ea772619cf1a32a4
kokonut93 <leekangil93@gmail.com>
2023-10-23 15:54:40 +0900

    similarity score matrix

---
+++ session_preprocess.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+import pickle
+
+from torchinfo import summary
+
+#1. Load data
+dir = './data'
+large_locale = 'UK'
+small_locale = 'ES'
+
+all_session = pd.read_csv(dir + '/sessions_train.csv')
+large_session = all_session[all_session['locale'] == large_locale]
+del all_session
+large_session
+
+#2. Processing as sesion dataset
+# Split the 'prev_items' column using space as the delimiter
+large_session['prev_items'] = large_session['prev_items'].str.split()
+# Remove the brackets from the first and last items
+large_session['prev_items'] = large_session['prev_items'].apply(lambda x: [item.strip("[]'") for item in x])
+# Create new columns and allocate each string from 'prev_items' to a different column
+max_length = large_session['prev_items'].str.len().max()
+for i in range(max_length):
+    large_session[i] = large_session['prev_items'].str[i]
+# Remove the 'prev_items' column
+large_session = large_session.drop('prev_items', axis=1)
+# Rename the 'next_item' column as 'valid'
+large_session = large_session.rename(columns={'next_item': 'valid'})
+# Remove the 'locale' column
+large_session = large_session.drop('locale', axis=1)
+# Move the 'valid' column as the last column
+valid_column = large_session['valid']
+large_session = large_session.drop('valid', axis=1)
+large_session['valid'] = valid_column
+
+#3. Selecting only users with purchase over "threshold"
+'''Need to decide on the threshold. The threshold entails "the minimum number of items that the user bought"'''
+threshold=10
+# Drop rows with NaN values after column 10
+large_session = large_session.dropna(axis=0, thresh=threshold)
+# Reset the index
+large_session = large_session.reset_index(drop=True)
--- similarity_matrix_generator.py
+++ similarity_matrix_generator.py
@@ -1,3 +1,18 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+import pickle
+
+from torchinfo import summary
+
+from sentence_transformers import SentenceTransformer, util
+import torch
+
 '''this pyfile generates the similarity matrix based on the below information
 1. meta-information of items in Region A (with size n)
 2. meta-information of items in Region B (with size m)
@@ -8,6 +23,89 @@ By doing so, we can create matrix where for each item in Region A,
 similarity scores for all items across Region B are calculated.
 
 
+
 Few dependencies exist.
 1. The number of items in both Region A, and B should be considered.
+
+
+A meta-information dataframe looks like this.
+Region A
+#################################################################################################################################
+||                                                                                                                             ||
+||id	        locale	title	                 price	    brand	          color	  size  model   material  author	desc   ||
+||B0BCFTG541	JP	    ワタシガモテナイノハドウカ...	600.0	   スクウェア・エニックス   NaN	    NaN	  NaN	  NaN	    タニガワニコ NaN     ||
+#################################################################################################################################
+
+Region B
+
+#######################################################################################################################################################
+||                                                                                                                                                   ||
+||id	        locale	title	                 price	    brand       color	  size  model   material  author	desc                             ||
+||09G6JM5WM	ES	Jonami Decoracion Halloween...   12.99	    Jonami	    Naranja	  NaN   NaN     Plástico  NaN	    🎃 ATMÓSFERA MÁXIMA DE HALLOWEEN ||
+#######################################################################################################################################################
 '''
+
+#1. Load data
+dir = './data'
+large_locale = 'JP'
+small_locale = 'ES'
+
+all_item = pd.read_csv(dir + './products_train.csv')
+large_item = all_item[all_item['locale'] == large_locale]
+small_item = all_item[all_item['locale'] == small_locale]
+del all_item
+
+with open(dir + f'{large_locale}_unique_values.pkl', 'rb') as f:
+    unique_values = pickle.load(f)
+
+
+#2. Selecting only items in Region A that were bought "above threshold"
+# Filter rows based on 'id' column values
+large_item_filtered = large_item[large_item['id'].isin(unique_values)]
+large_item_filtered = large_item_filtered.reset_index(drop=True)
+
+
+#3. Processing data - concat title and desc
+large_item_filtered['title'] = large_item_filtered['title'].fillna("blank").str.replace('|', '-*-')
+small_item['desc'] = small_item['desc'].fillna("blank").str.replace('|', '-')
+
+has_delimiter = large_item_filtered['desc'].str.contains('|', regex=False)
+rows_with_delimiter = large_item_filtered['desc'][has_delimiter]
+has_delimiter = small_item['desc'].str.contains('|', regex=False)
+rows_with_delimiter = small_item['desc'][has_delimiter]
+
+#corpus(Region B) and queries(Region A)
+corpus = (small_item['title'] + ' | ' + small_item['desc']).tolist()
+queries = (large_item_filtered['title'] + ' | ' + large_item_filtered['desc']).tolist()
+
+
+#4. Calculate similarity matrix
+# Load Sentence Transformer (SBERT)
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2', device='cuda')
+#embed corpus
+corpus_embedding = model.encode(corpus, convert_to_tensor=True)
+top_k = min(200, len(corpus))
+
+# Since the size of matrix is too large to load on RAM, save the matrix on local using memmap
+large_to_small_mem = np.memmap(filename = f'./large/{large_locale}_to_{small_locale}_mem', dtype='float32', mode='w+', shape=(len(queries), len(corpus)))
+#
+for idx, query in enumerate(tqdm(queries)):
+    query_embedding = model.encode(query, convert_to_tensor=True)
+    cos_scores = util.cos_sim(query_embedding, corpus_embedding)[0]
+    large_to_small_mem[idx] = cos_scores.cpu().numpy()
+#save
+large_to_small_mem.flush()
+
+#since there are duplicating items in Region A, and B, the similarity score bewteen the same items is calculated as 1.0
+# Find the common values in small_item['id'] and large_item['id']
+common_ids = list(set(small_item['id']).intersection(set(large_item_filtered['id'])))
+# Print the common values list
+print(f'duplicate count : {len(common_ids)}')
+
+large_to_small_duplicate_idx = dict()
+for item in tqdm(common_ids):
+    large_to_small_duplicate_idx[large_item_filtered[large_item_filtered['id'] == item].index[0]] = small_item[small_item['id'] == item].index[0]
+for key, value in large_to_small_duplicate_idx.items():
+    large_to_small_mem[key][value] = float(1.0)
+#save
+large_to_small_mem.flush()
